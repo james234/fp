3131ff72efc7841cd0ef98af05e95ed328949b05
james234 <mithundhiman@gmail.com>
2016-02-12 15:02:19 +0530

    Create home.php

---
+++ view/home.php
@@ -0,0 +1,129 @@
+<!-- Modal -->
+<div class="presentation-container">
+        	<div class="container">
+        		<div class="row">
+	        		<div class="col-sm-12 wow fadeInLeftBig animated" style="visibility: visible; animation-name: fadeInLeftBig;">
+	            		<h1 style="color:#2dcc70">Lorem Ipsum Dolor Sit Amet</h1>
+	            		<p>We design beautiful websites, logos and prints. Your project is safe with us.</p>
+	            	</div>
+            	</div>
+        	</div>
+        </div>
+
+
+<div class="services-container">
+<div class="container">
+ <div class = "col-sm-6 col-md-4">
+      <div class = "thumbnail">
+         <img src="<?php echo base_url()?>assets/images/work1.jpg" alt = "Generic placeholder thumbnail">
+
+      
+      <div class = "caption">
+         <h3 style="text-align:center;">Jyotisar</h3>
+         <p>Jyotisar is a town on the Kurukshetra-Pehowa road, 5 km west of Thanesar in the Kurukshetra district of Haryana 'Jyoti' means light and 'Sar' means the core meaning. Hence the name of place is a</p>
+         
+         <p style="margin:0 auto; text-align:center;">
+            <a href = "#" class = "btn btn-primary" role = "button">
+               Read more
+            </a> 
+            
+         </p>
+               </div>
+      </div>
+   </div><!---- close col-md-4---->
+
+<div class = "col-sm-6 col-md-4">
+      <div class = "thumbnail">
+         <img src="<?php echo base_url()?>assets/images/work1.jpg" alt = "Generic placeholder thumbnail">
+
+      
+      <div class = "caption">
+         <h3 style="text-align:center;">Srikrishna Museum</h3>
+         <p>Every museum has its own character and personality. With a view to awakening the people morally and culturally through the ideas and ideals of Krishna and to enlighten the people </p>
+         
+         <p style="margin:0 auto; text-align:center;">
+            <a href = "#" class = "btn btn-primary" role = "button">
+               Read more
+            </a> 
+            
+         </p>
+               </div>
+      </div>
+   </div><!---- close col-md-4---->
+   <div class = "col-sm-6 col-md-4">
+      <div class = "thumbnail">
+         <img src="<?php echo base_url()?>assets/images/work1.jpg" alt = "Generic placeholder thumbnail">
+
+      
+      <div class = "caption">
+         <h3 style="text-align:center;">Brahma Sarovar</h3>
+         <p>Brahma Sarovar is an ancient water tank sacred to Hinduism in Thanesar, in the state of Haryana in North India.[1] Hinduism lays emphasis on taking bath for internal and external purity. Most religiou</p>
+         
+         <p style="margin:0 auto; text-align:center;">
+            <a href = "#" class = "btn btn-primary" role = "button">
+               Read more
+            </a> 
+            
+         </p>
+               </div>
+      </div>
+   </div><!---- close col-md-4---->
+
+<div class = "col-sm-6 col-md-4">
+      <div class = "thumbnail">
+         <img src="<?php echo base_url()?>assets/images/work1.jpg" alt = "Generic placeholder thumbnail">
+
+      
+      <div class = "caption">
+         <h3 style="text-align:center;">Sthaneshawar Mahadev Mandir</h3>
+         <p>Brahma Sarovar is an ancient water tank sacred to Hinduism in Thanesar, in the state of Haryana in North India.[1] Hinduism lays emphasis on taking bath for internal and external purity. Most religiou</p>
+         
+         <p style="margin:0 auto; text-align:center;">
+            <a href = "#" class = "btn btn-primary" role = "button">
+               Read more
+            </a> 
+            
+         </p>
+               </div>
+      </div>
+   </div><!---- close col-md-4---->
+   <div class = "col-sm-6 col-md-4">
+      <div class = "thumbnail">
+         <img src="<?php echo base_url()?>assets/images/work1.jpg" alt = "Generic placeholder thumbnail">
+
+      
+      <div class = "caption">
+         <h3 style="text-align:center;">Kurukshetra Panorama & Science Centre</h3>
+         <p>Brahma Sarovar is an ancient water tank sacred to Hinduism in Thanesar, in the state of Haryana in North India.[1] Hinduism lays emphasis on taking bath for internal and external purity. Most religiou</p>
+         
+         <p style="margin:0 auto; text-align:center;">
+            <a href = "#" class = "btn btn-primary" role = "button">
+               Read more
+            </a> 
+            
+         </p>
+               </div>
+      </div>
+   </div><!---- close col-md-4---->
+   <div class = "col-sm-6 col-md-4">
+      <div class = "thumbnail">
+         <img src="<?php echo base_url()?>assets/images/work1.jpg" alt = "Generic placeholder thumbnail">
+
+      
+      <div class = "caption">
+         <h3 style="text-align:center;">Kurukshetra Panorama & Science Centre</h3>
+         <p>Brahma Sarovar is an ancient water tank sacred to Hinduism in Thanesar, in the state of Haryana in North India.[1] Hinduism lays emphasis on taking bath for internal and external purity. Most religiou</p>
+         
+         <p style="margin:0 auto; text-align:center;">
+            <a href = "#" class = "btn btn-primary" role = "button">
+               Read more
+            </a> 
+            
+         </p>
+               </div>
+      </div>
+   </div><!---- close col-md-4---->
+
+</div><!---- close conatiner--->
+</div><!----- close service container--->
+
